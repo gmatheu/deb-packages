6245911e79c55a57de178d04c4928171130ed6ee
Gonzalo Matheu <gmatheu@thoughtworks.com>
2015-01-22 16:51:17 -0200

    Adding intellijs pre-packager script

---
+++ intellij-idea/build.rb
@@ -0,0 +1,19 @@
+#! /usr/bin/env ruby
+
+require 'pre-packager'
+
+include PrePackager
+Application.new(name: 'ideaIC-$version',
+  version: '14',
+  minor_version: '0.2').
+    download_from 'http://download-cf.jetbrains.com/idea/$name.$minor_version.tar.gz' do |p|
+    p.extract('/opt', strip: 1, use_name: true) do |content|
+      content.link 'bin/idea.sh', use_name: true
+    end
+  end.
+  with_templates('template/usr/local/share/applications/*', '/usr/local/share/applications').
+  create_package(dry_run: false,
+                  version: '$version.$minor_version',
+                  description: 'IntelliJ Idea $version Community Edition',
+                  maintainer: 'Gonzalo Matheu <gonzalommj@gmail.com>')
+
