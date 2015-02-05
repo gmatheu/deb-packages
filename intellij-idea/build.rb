12a79a1f912bc18a43ab8dc5b8ce4e1fd41426fa
Gonzalo Matheu <gmatheu@thoughtworks.com>
2015-02-05 13:17:00 -0200

    Parameterizing idea build

--- intellij-idea/build.rb
+++ intellij-idea/build.rb
@@ -1,19 +1,39 @@
 #! /usr/bin/env ruby
 
+require 'optparse'
 require 'pre-packager'
 
 include PrePackager
+
+options={major: '14',
+         minor: '0.3'}
+OptionParser.new do |opts|
+  opts.banner = "Usage: #{$0} [--major 14] [--minor 0.3]"
+  opts.on('--major major', 'Major Version') do |major|
+    options[:major] = major.to_s
+  end
+
+  opts.on('--minor minor', 'Minor Version') do |minor|
+    options[:minor] = minor.to_s
+  end
+
+  opts.on('-h', '--help', 'Displays Help') do
+    puts opts
+    exit
+  end
+end.parse!
+
 Application.new(name: 'ideaIC-$version',
-  version: '14',
-  minor_version: '0.3').
-    download_from 'http://download-cf.jetbrains.com/idea/$name.$minor_version.tar.gz' do |p|
-    p.extract('/opt', strip: 1, use_name: true) do |content|
-      content.link 'bin/idea.sh', use_name: true
-    end
-  end.
-  with_templates('template/usr/local/share/applications/*', '/usr/local/share/applications').
-  create_package(dry_run: false,
-                  version: '$version.$minor_version',
-                  description: 'IntelliJ Idea $version Community Edition',
-                  maintainer: 'Gonzalo Matheu <gonzalommj@gmail.com>')
+                version: options[:major],
+                minor_version: options[:minor]).
+download_from 'http://download-cf.jetbrains.com/idea/$name.$minor_version.tar.gz' do |p|
+                  p.extract('/opt', strip: 1, use_name: true) do |content|
+                    content.link 'bin/idea.sh', use_name: true
+                  end
+                end.
+                with_templates('template/usr/local/share/applications/*', '/usr/local/share/applications').
+                create_package(dry_run: false,
+                               version: '$version.$minor_version',
+                               description: 'IntelliJ Idea $version Community Edition',
+                               maintainer: 'Gonzalo Matheu <gonzalommj@gmail.com>')
 
