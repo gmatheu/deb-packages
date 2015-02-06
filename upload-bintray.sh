ee0e3326ea9a63583bf5656d4e5f2bfb037be308
Gonzalo Matheu <gmatheu@thoughtworks.com>
2015-02-06 17:46:24 -0200

    Adding gpg key to upload

--- upload-bintray.sh
+++ upload-bintray.sh
@@ -1,6 +1,12 @@
 #! /bin/bash
 
+echo -n GPG Passphrase: 
+read -s GPG_PASSPHRASE
+echo
+
 function upload() {
   URL="https://api.bintray.com/content/$BINTRAY_USER/deb/$PACKAGE/$VERSION/$DEB"
-  curl -vT $DEB -u$BINTRAY_USER:$BINTRAY_API_KEY $URL
+  curl -vT $DEB -u$BINTRAY_USER:$BINTRAY_API_KEY $URL \
+    -H 'X-Bintray-Publish: 1' \
+    -H "X-GPG-PASSPHRASE: $GPG_PASSPHRASE" 
 }
