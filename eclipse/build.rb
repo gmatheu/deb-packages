505fafecb537b882da9ee1c2be4d3505661ab2e2
Gonzalo Matheu <gonzalommj@gmail.com>
2015-12-29 17:15:02 -0300

    Adding eclipse mars

--- eclipse/build.rb
+++ eclipse/build.rb
@@ -3,11 +3,12 @@
 require 'pre-packager'
 
 include PrePackager
-url = 'http://eclipse.c3sl.ufpr.br/technology/epp/downloads/release/luna/SR1a/eclipse-jee-luna-SR1a-linux-gtk-x86_64.tar.gz'
+file = 'release/mars/1/eclipse-jee-mars-1-linux-gtk-x86_64.tar.gz'
+url = "http://eclipse.c3sl.ufpr.br/technology/epp/downloads/#{file}"
 Application.new(name: '$basename-$version',
   basename: 'eclipse',
-  version: 'luna',
-  minor_version: '4.4.1').
+  version: 'mars',
+  minor_version: '4.5.1').
   download_from url do |p|
     p.extract('/opt/$name', strip: 1) do |content|
       content.link '$name/eclipse', use_name: true
