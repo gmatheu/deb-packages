0950e60751874d2844ec7473dc94cc854b698775
Gonzalo Matheu <gonzalommj@gmail.com>
2017-10-24 13:23:57 -0300

    git-crypt 0.5.0

--- git-crypt/build.sh
+++ git-crypt/build.sh
@@ -3,23 +3,21 @@
 TMP=`pwd`/tmp
 BIN=$TMP/usr/local/bin
 PACKAGE=git-crypt
-VERSION=0.4.2
+VERSION=0.5.0
 
 mkdir -p $BIN
 sudo docker build -t $PACKAGE .
-sudo docker run -e BRANCH=$VERSION -v $BIN:/output git-crypt 
+sudo docker run -e BRANCH=$VERSION -v $BIN:/output git-crypt
 
 fpm -s dir -t deb -C $TMP -n $PACKAGE -v $VERSION -a amd64 \
   --description "Transparent file encryption in git" \
   --maintainer "Gonzalo Matheu <gonzalommj@gmail.com>" \
   --deb-build-depends "git (>= 1.7.2)" \
   --provides git-crypt \
-  usr 
+  usr
 
 DEB="${PACKAGE}_${VERSION}_amd64.deb"
 rm -rf $TMP
 
-. ../.secrets
-. ../upload-bintray.sh
-
-upload
+. ../bintray.sh
+bintray-upload
