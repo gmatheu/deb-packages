8abc1bb75dee34e54d6553e7a282fa04e78d5a0e
Gonzalo Matheu <gonzalommj@gmail.com>
2017-11-06 14:17:40 -0300

    geckodriver 0.19.1

--- geckodriver/build.sh
+++ geckodriver/build.sh
@@ -14,17 +14,18 @@ you_need "unzip"
 build() {
   VERSION=$1
   BITS=64
-  ARCH=linux$BITS
+  ARCH=amd${BITS}
+  FILE_ARCH=linux$BITS
   TMP=./tmp-$VERSION
   PACKAGE=geckodriver
   DEST=$TMP/opt/$PACKAGE
   DEB=${PACKAGE}_${VERSION}_${ARCH}.deb
-  FILE=${PACKAGE}-v${VERSION}-${ARCH}.tar.gz
+  FILE=${PACKAGE}-v${VERSION}-${FILE_ARCH}.tar.gz
   LINK=$TMP/usr/local/bin/$PACKAGE
 
   echo "Downloading geckodriver $VERSION for $ARCH"
 
-  wget -nc https://github.com/mozilla/geckodriver/releases/download/v${VERSION}/${FILE} -O ${FILE} || echo "Already downloaded"
+  wget --continue -nc https://github.com/mozilla/geckodriver/releases/download/v${VERSION}/${FILE} -O ${FILE} || echo "Already downloaded"
   mkdir --parents $DEST
   tar -xf $FILE -C $DEST
   mkdir --parents $TMP/usr/local/bin
@@ -32,15 +33,15 @@ build() {
   ln -sf /opt/${PACKAGE}/${PACKAGE} $LINK
 
   rm -f $DEB
-  fpm -s dir -t deb -C $TMP -n ${PACKAGE} -v $VERSION -a amd${BITS} \
+  fpm -s dir -t deb -C $TMP -n ${PACKAGE} -v $VERSION -a ${ARCH} \
     --description "Proxy for using W3C WebDriver-compatible clients to interact with Gecko-based browsers." \
     --maintainer "Gonzalo Matheu <gonzalommj@gmail.com>" \
     --provides ${PACKAGE} \
     usr opt
 
   . ../bintray.sh
-  # bintray-create-version
-  # bintray-upload
+  bintray-upload
 }
 
-build "0.13.0"
+# build "0.13.0"
+build "0.19.1"
